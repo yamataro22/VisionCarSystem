7bae6e9e9132946fcc139163cd4e8f77621c833e
Adam Wojtysiak <41833220+yamataro22@users.noreply.github.com>
2018-11-15 16:59:32 +0100

    Fist commit

---
+++ app/src/main/java/com/example/adamw/visoncarsystem/CameraBasicActivity.java
@@ -0,0 +1,12 @@
+package com.example.adamw.visoncarsystem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraBasicActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
